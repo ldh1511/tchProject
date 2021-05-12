a271560d867b230bbdfb64e144720d6a46438979
ldh1511 <53638313+ldh1511@users.noreply.github.com>
2021-05-13 02:18:02 +0700

    update sort

--- src/components/CategoryPage.js
+++ src/components/CategoryPage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import Header from './Header';
 import Item from './Item';
@@ -6,17 +6,34 @@ import Item from './Item';
 function CategoryPage(props) {
     const product = useSelector(state => state.product.data);
     const title = useSelector((state) => state.product.type);
+    const [sort, setSort] = useState("");
     let slug = props.location.pathname;
     slug = slug.split('/');
     let match = product.filter(e => e[1].type === slug[slug.length - 1]);
+    const [items, setItems] = useState(match);
     const getTitle = () => {
         let titleArr = Object.entries(title);
         let x = titleArr.filter((ele) => ele[1].id === match[0][1].type);
         return x[0][1].name;
     }
     const getItem = () => {
-        return match.map((e, i) => <Item data={e} key={i} />)
+        return items.map((e, i) => <Item data={e} key={i} />)
     }
+    const handleSortItems = (e) => {
+        let result;
+        if (e.target.value === "up") {
+            result = match.sort((a, b) => a[1].price - b[1].price);
+        }
+        if (e.target.value === "down") {
+            result = match.sort((a, b) => b[1].price - a[1].price);
+        }
+        setSort(result);
+    }
+    useEffect(() => {
+        if (sort !== "") {
+            setItems(sort)
+        }
+    }, [sort])
     return (
         <>
             <Header />
@@ -29,9 +46,9 @@ function CategoryPage(props) {
                         </div>
                     </div>
                     <div className="category-block-middle">
-                        <select className="sort-box">
-                            <option>Giá tăng dần</option>
-                            <option>Giá giảm dần</option>
+                        <select className="sort-box" onClick={(e) => handleSortItems(e)}>
+                            <option value="up">Giá tăng dần</option>
+                            <option value="down">Giá giảm dần</option>
                         </select>
                     </div>
                     <div className="category-block-bottom">
