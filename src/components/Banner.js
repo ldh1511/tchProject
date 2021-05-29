c8e2aada85f780a1b959998a472b92305a02aff0
ldh1511 <53638313+ldh1511@users.noreply.github.com>
2021-05-29 09:03:03 +0700

    update category

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -11,7 +11,7 @@ const Banner=(props)=>{
             <i className="fas fa-bars menu-icon" onClick={handleClick}></i>
             <img src={Image} alt=""></img>
             <div className="banner-content">
-                <h1>JIZ'COFEE</h1>
+                <h1>JIZ'COFFEE</h1>
                 <p>All you need to feel better is coffee</p>
             </div>
         </div>
