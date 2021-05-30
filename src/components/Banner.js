81a00fee209dad8f5a6dd1f214754858c8413788
ldh1511 <53638313+ldh1511@users.noreply.github.com>
2021-05-30 21:10:55 +0700

    update

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
-import Image from './../img/background.jpg';
+import Image from './../img/scroll.gif';
 const Banner = ({ img, title }) => {
     console.log(title);
     const dispatch = useDispatch();
@@ -18,7 +18,7 @@ const Banner = ({ img, title }) => {
             }
         }>
             <i className="fas fa-bars menu-icon" onClick={handleClick}></i>
-            {/* <img src={Image} alt=""></img> */}
+            <img src={Image} alt=""></img>
             <div className="banner-content">
                 <h1>{title}</h1>
                 <p>All you need to feel better is coffee</p>
